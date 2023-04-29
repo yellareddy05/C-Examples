aa4e8bf1e9eb2a87a3894ab89992706b0e91af99
yellareddy05 <yella0511@gmail.com>
2023-04-29 07:49:45 +0530

    commiting 18.c and example

---
+++ 18.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+struct VECTOR{
+	char a,b;
+	int i;
+	char x;
+	char y;
+}YellaVECTOR;
+
+int main()
+{
+	printf("size is %lu",sizeof(YellaVECTOR));
+	return 0;
+}
+
+----------------------------------------
+What will be the output, explain in detail?
+
+								Yella Reddy....
