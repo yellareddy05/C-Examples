f24ef1e01d90ce489382826f19614e8784badc25
yellareddy05 <yella0511@gmail.com>
2023-05-03 09:06:39 +0530

    Commiting Day_24.c

---
+++ Day24.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+void VECTOR(void);
+
+int main()
+{
+	goto YellaVECTOR;
+	VECTOR();
+	return 0;
+}
+
+void VECTOR(void)
+{
+YellaVECTOR;
+printf("#YellaVECTOR");
+}
+
+----------------------------------------
+What will be the output, explain in detail?
+
+								Yella Reddy....
