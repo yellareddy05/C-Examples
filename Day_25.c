7574ac1b517fbd81827e78527583107b9d6f3a9c
yellareddy05 <yella0511@gmail.com>
2023-05-03 09:22:57 +0530

    Commiting Day_25.c

---
+++ Day_25.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct VECTOR{
+int x=20;
+char y=c;
+float z=5.6;
+}
+int main()
+{
+	struct VECTOR Yella;
+	printf("%f",Yella.z);
+	printf("%d",Yella.x);
+	return 0;
+}
+
+----------------------------------------
+What will be the output, explain in detail?
+
+								Yella Reddy....
